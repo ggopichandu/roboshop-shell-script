23e8f52c93670d20780247bd1cf4b97743d10a90
Gopichand <gummagopichand@gmail.com>
2026-02-15 17:42:07 +0530

    roboshop-shell

---
+++ cart.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+
+USERID=$(id -u)
+TIMESTAMP=$(date +%F-%H-%M-%S)
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+MONGO_HOST='mangodb.gopichand.online'
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2...$R FAILURE $N"
+        exit 1
+    else
+        echo -e "$2...$G SUCCESS $N"
+    fi        
+}
+
+if [ $USERID -ne 0 ]
+then
+    echo "Please run this script with root user"
+    exit 1
+else 
+    echo "You are super user"
+fi
+
+dnf module disable nodejs -y &>> $LOGFILE
+VALIDATE $? "Disabling current nodejs"
+
+dnf module enable nodejs:20 -y &>> $LOGFILE
+VALIDATE $? "Enabling nodejs:20"
+
+dnf install nodejs -y &>> $LOGFILE
+VALIDATE $? "Installing Nodejs"
+
+id roboshop &>> $LOGFILE
+if [ $? -ne 0 ]
+then
+    useradd roboshop &>> $LOGFILE
+    VALIDATE $? "Adding Roboshop user"
+else
+    echo -e "roboshop user already exist ...$Y SKIPPING $N"
+fi 
+
+rm -rf /app &>> $LOGFILE
+VALIDATE $? ""Clean up exisiting directory
+
+mkdir -p /app &>> $LOGFILE
+VALIDATE $? "Creating app directory"
+
+curl -o /tmp/cart.zip https://roboshop-builds.s3.amazonaws.com/cart.zip &>> $LOGFILE
+VALIDATE $? "Downloading Catalouge application"
+
+cd /app &>> $LOGFILE
+VALIDATE $? "Moving to app directory"
+
+unzip /tmp/cart.zip &>> $LOGFILE
+VALIDATE $? "Extracting cart"
+
+npm install &>> $LOGFILE
+VALIDATE $? "Install dpendencies"
+
+cp /home/ec2-user/roboshop-shell-script/cart.service /etc/systemd/system/cart.service &>> $LOGFILE
+VALIDATE $? "Copied cart service"
+
+systemctl daemon-reload &>> $LOGFILE
+VALIDATE $? "Daemon Reload"
+
+systemctl enable cart &>> $LOGFILE
+VALIDATE $? "Enable cart"
+
+systemctl start cart &>> $LOGFILE
+VALIDATE $? "Starting cart"
