9d9d695c6346132afba11d83e5f3071fc49be4e7
Gopichand <gummagopichand@gmail.com>
2026-02-15 13:09:19 +0530

    roboshop-shell

---
+++ create-ec2.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+instances=("mangodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "web")
+
+for name in ${instances(@)}; do
+if [ $name == "shipping" ] || [ $name == "mysql" ]
+then
+    instance_type="t3.medium"
+else
+    instance_type="t3.micro"
+fi
+echo "Creating instance for: $name with instance type: $instance_type"
+instance_id=$(aws ec2 run-instances --image-id ami-0220d79f3f480ecf5 --instance-type $instance_type --security-group-ids sg-046836072ceb917dd --subnet-id subnet-0546bcf98efcaa4a4
+--query "Instances[0].InstanceId" --output text)        
