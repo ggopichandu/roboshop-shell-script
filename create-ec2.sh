a8215de38c5558052dc1a0a4e1617ee7c11d1d6c
Gopichand <gummagopichand@gmail.com>
2026-02-15 13:35:03 +0530

    roboshop-shell

--- create-ec2.sh
+++ create-ec2.sh
@@ -11,4 +11,9 @@ for name in ${instances[@]}; do
     fi
     echo "Creating instance for: $name with instance type: $instance_type"
     instance_id=$(aws ec2 run-instances --image-id ami-0220d79f3f480ecf5 --instance-type $instance_type --security-group-ids sg-046836072ceb917dd --subnet-id subnet-0546bcf98efcaa4a4 --query 'Instances[0].InstanceId' --output text)
+
+    aws ec2 create-tags --resources $instance_id --tags Key=Name,Value=$name
+
+    iprivate_ip=$(aws ec2 describe-instances --instance-ids $instance_id --query 'Reservations[0].Instances[0].[PrivateIpAddress]' --output text)
+    
 done
